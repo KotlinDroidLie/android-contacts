4cee4764d68122c1827f0940453245ba926edee3
KotlinDroidLie <andrejkifa@gmail.com>
2025-09-03 13:17:08 +0300

    Практическая 2

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/model/FilterContactTypeUi.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/model/FilterContactTypeUi.java
@@ -2,7 +2,9 @@ package ru.yandex.practicum.contacts.presentation.filter.model;
 
 import androidx.annotation.NonNull;
 
-public class FilterContactTypeUi {
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
+
+public class FilterContactTypeUi implements ListDiffInterface<FilterContactTypeUi> {
 
     private final FilterContactType contactType;
     private final boolean selected;
@@ -11,6 +13,12 @@ public class FilterContactTypeUi {
         this.contactType = contactType;
         this.selected = selected;
     }
+
+    @Override
+    public boolean theSameAs(FilterContactTypeUi elementFilterContactTypeUI) {
+        return this.getContactType() == elementFilterContactTypeUI.getContactType();
+    }
+
 
     public FilterContactType getContactType() {
         return contactType;
